02395ac35f685664b617e40eddc4d124a9aa825c
Andre Herter <andre-herter@web.de>
2025-03-01 12:35:42 +0100

    first commit

---
+++ notesAPI.js
@@ -0,0 +1,62 @@
+const LOCAL_STORAGE_KEY = "notes-app";
+
+function getNotes() {
+  return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || [];
+}
+
+function saveNote(title, content, id = undefined) {
+  const notes = getNotes();
+
+  if (!id) {
+    notes.push({
+      title,
+      content,
+      id: getNextId(),
+      lastUpdated: new Date().getTime(),
+    });
+  } else {
+    const indexOffNoteWithId = notes.findIndex((note) => note.id === id);
+
+    if (indexOffNoteWithId > -1) {
+      notes[indexOffNoteWithId] = {
+        title,
+        content,
+        id: id,
+        lastUpdated: new Date().getTime(),
+      };
+    }
+  }
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(notes));
+}
+
+function deleteNote(id) {
+  if (!id) return;
+
+  const notes = getNotes();
+
+  const filteredNotes = notes.filter((note) => note.id !== Number(id));
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(filteredNotes));
+}
+
+function deleteAllNotes() {
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  // localStorage.clear();
+}
+
+function getNextId() {
+  const notes = getNotes();
+
+  const sortedNotes = notes.sort((noteA, noteB) => noteA.id - noteB.id);
+
+  let nextId = 1;
+
+  for (let note of sortedNotes) {
+    if (nextId < note.id) break;
+
+    nextId = note.id + 1;
+  }
+
+  return nextId;
+}
---
+++ script.js
@@ -0,0 +1,155 @@
+const notesListEl = document.querySelector(".notes-list");
+const saveButtonEl = document.querySelector(".save-btn");
+const createNewBtnEl = document.querySelector(".create-new-btn");
+const deleteButtonEl = document.querySelector(".delete-btn");
+const deleteAllNotesButtonEl = document.querySelector(".delete-all-notes-btn");
+const titleInputEl = document.getElementById("title-input");
+const contentInputEl = document.getElementById("text-input");
+
+saveButtonEl.addEventListener("click", clickSaveButton);
+createNewBtnEl.addEventListener("click", newNote);
+deleteButtonEl.addEventListener("click", clickDeleteButton);
+deleteAllNotesButtonEl.addEventListener("click", clickDeleteAllNotes);
+
+displayNotesList();
+applyListeners();
+removeDeleteAllNotesButton();
+
+function applyListeners() {
+  const noteEntriesEls = document.querySelectorAll(".note-entry");
+
+  noteEntriesEls.forEach((noteEntry) => {
+    noteEntry.addEventListener("click", () =>
+      selectNode(noteEntry.getAttribute("data-id"))
+    );
+  });
+}
+
+function displayNotesList() {
+  const notes = getNotes();
+
+  const sortedNotes = notes.sort(
+    (noteA, noteB) => noteB.lastUpdated - noteA.lastUpdated
+  );
+
+  let html = "";
+
+  sortedNotes.forEach((note) => {
+    html += `
+    <div class="note-entry" data-id="${note.id}">
+        <div class="note-title">${escapeHtml(note.title)}</div>
+        <div class="note-content-teaser">${escapeHtml(note.content)}</div>
+        <div class="note-date">${new Date(note.lastUpdated).toLocaleString(
+          "de-DE"
+        )}</div>
+    </div>
+
+`;
+  });
+
+  notesListEl.innerHTML = html;
+}
+
+function clickSaveButton() {
+  const title = titleInputEl.value;
+  const content = contentInputEl.value;
+
+  if (!title || !content) {
+    alert("Bitte Titel und Inhalt eingeben");
+    return;
+  }
+
+  saveNote(title, content, Number(getCurrentlySelectedId()));
+
+  titleInputEl.value = "";
+  contentInputEl.value = "";
+
+  displayNotesList();
+  applyListeners();
+  removeDeleteAllNotesButton();
+}
+
+function clickDeleteButton() {
+  const currentlySelectedId = getCurrentlySelectedId();
+
+  if (!currentlySelectedId) return;
+
+  deleteNote(currentlySelectedId);
+
+  titleInputEl.value = "";
+  contentInputEl.value = "";
+
+  displayNotesList();
+  applyListeners();
+  removeDeleteAllNotesButton();
+}
+
+function clickDeleteAllNotes() {
+  deleteAllNotes();
+
+  displayNotesList();
+  applyListeners();
+  removeDeleteAllNotesButton();
+}
+
+function removeDeleteAllNotesButton() {
+  if (notesListEl.children.length === 0) {
+    deleteAllNotesButtonEl.style.display = "none";
+  } else {
+    deleteAllNotesButtonEl.style.display = "flex";
+  }
+}
+
+function selectNode(id) {
+  const selectedNodeEl = document.querySelector(`.note-entry[data-id="${id}"]`);
+
+  if (selectedNodeEl.classList.contains("selected-note")) return;
+
+  removeSelectedClass();
+
+  selectedNodeEl.classList.add("selected-note");
+
+  const notes = getNotes();
+
+  const selectedNode = notes.find((note) => note.id === Number(id));
+
+  if (!selectNode) return;
+
+  titleInputEl.value = selectedNode.title;
+  contentInputEl.value = selectedNode.content;
+}
+
+function newNote() {
+  titleInputEl.value = "";
+  contentInputEl.value = "";
+
+  removeSelectedClass();
+}
+
+function removeSelectedClass() {
+  const noteEntriesEls = document.querySelectorAll(".note-entry");
+  noteEntriesEls.forEach((noteEntry) => {
+    noteEntry.classList.remove("selected-note");
+  });
+}
+
+function getCurrentlySelectedId() {
+  let currentId = undefined;
+
+  const currentlySelectedNoteEl = document.querySelector(".selected-note");
+
+  if (currentlySelectedNoteEl) {
+    currentId = currentlySelectedNoteEl.getAttribute("data-id");
+  }
+
+  return currentId;
+}
+
+function escapeHtml(unsafe) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#039;");
+}
